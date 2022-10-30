33a97486104049c20fb49af87c16bd196ea1b1c6
y1qu <114897758+y1qu@users.noreply.github.com>
2022-10-30 04:50:09 -0700

    update img

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,18 +4,18 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  const selectElement = document.querySelector('.horn');
+  const selectElement = document.querySelector('.horn-select');
   
   selectElement.addEventListener('change', (event) => {
     let image = document.querySelectorAll('img')[0]
-    if(event.target.value == air-horn){
-      image.src = "air-horn.svg"
+    if(event.target.value == 'air-horn'){
+      image.src = "assets/images/air-horn.svg"
     }
-    else if (event.target.value == car-horn){
-      image.src = "car-horn.svg"
+    else if (event.target.value == 'car-horn'){
+      image.src = "/assets/images/car-horn.svg"
     }
-    else if (event.target.value == party-horn){
-      image.src = "party-horn.svg"
+    else if (event.target.value == 'party-horn'){
+      image.src = "./assets/images/party-horn.svg"
     }
   });
 
