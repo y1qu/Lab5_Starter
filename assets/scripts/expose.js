1c5fdf638518b61db5530062ef712c47ba524bbe
y1qu <114897758+y1qu@users.noreply.github.com>
2022-10-30 04:43:03 -0700

    update img select

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,19 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const selectElement = document.querySelector('.horn');
+  
+  selectElement.addEventListener('change', (event) => {
+    let image = document.querySelectorAll('img')[0]
+    if(event.target.value == air-horn){
+      image.src = "air-horn.svg"
+    }
+    else if (event.target.value == car-horn){
+      image.src = "car-horn.svg"
+    }
+    else if (event.target.value == party-horn){
+      image.src = "party-horn.svg"
+    }
+  });
+
 }
